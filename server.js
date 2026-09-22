9b26c5603ed833ee4c436bd1657dd1a8f034a31f
Mauricio Andrelino <mauricioandrelino004@gmail.com>
2026-09-21 21:01:32 -0300

    sever

--- server.js
+++ server.js
@@ -2,10 +2,29 @@ const express = require('express');
 const app = express()
 const port = 3000
 
+const filmes = [
+  { id: 1, title: 'Harry Potter', author: 'J.K. Rowling' },
+  { id: 2, title: 'Senhor dos Anéis', author: 'J.R.R. Tolkiens' },
+  { id: 3, title: 'O Hobbit', author: 'J.R.R. Tolkiens' }
+]
+
 app.get('/', (req, res) => {
-  res.send('Ola Mundo!')
+  res.send('API-BOOK está funcionando!')
+})
+
+
+app.get('/filmes/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+
+  const filme = filmes.find(f => f.id === id)
+ 
+if (!filmes) {
+  return res.status(404).send('Filme não encontrado')
+}
+
+res.json(filme)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`API-BOOK rodando na porta ${port}`)
 })
